0f00bd9a90ec85cac8cc0cb3c355a80c368b5f6e
Kamran Walsh <149266651+KamranWalsh@users.noreply.github.com>
2024-12-09 09:41:52 -0500

    Add performance metrics, alternative RDM output

---
+++ mse_extra/performancemetrics_test/splitsimtest_older.sh
@@ -0,0 +1,43 @@
+
+#oldest sinatra
+
+cd ~/Desktop/FlounderMSE/kw_sims_test/TestingSims/testsims_old/SplittingSimsTest/runnewest50sims26years/spawbiotest_oldersinatra2bag
+./sinatra.exe 
+
+#wait
+
+cd ~/Desktop/FlounderMSE/kw_sims_test/TestingSims/testsims_old/SplittingSimsTest/runnewest50sims26years/split_oldersinatra2bag/sim1_10
+./sinatra.exe &
+cd ~/Desktop/FlounderMSE/kw_sims_test/TestingSims/testsims_old/SplittingSimsTest/runnewest50sims26years/split_oldersinatra2bag/sim11_20
+./sinatra.exe & 
+cd ~/Desktop/FlounderMSE/kw_sims_test/TestingSims/testsims_old/SplittingSimsTest/runnewest50sims26years/split_oldersinatra2bag/sim21_30
+./sinatra.exe 
+
+wait
+
+cd ~/Desktop/FlounderMSE/kw_sims_test/TestingSims/testsims_old/SplittingSimsTest/runnewest50sims26years/split_oldersinatra2bag/sim31_40
+./sinatra.exe & 
+cd ~/Desktop/FlounderMSE/kw_sims_test/TestingSims/testsims_old/SplittingSimsTest/runnewest50sims26years/split_oldersinatra2bag/sim41_50
+./sinatra.exe
+
+wait
+
+cd ~/Desktop/FlounderMSE/kw_sims_test/TestingSims/testsims_old/SplittingSimsTest/runnewest50sims26years/spawbiotest_oldersinatra2all
+./sinatra.exe 
+
+wait
+
+cd ~/Desktop/FlounderMSE/kw_sims_test/TestingSims/testsims_old/SplittingSimsTest/runnewest50sims26years/split_oldersinatra2all/sim1_10
+./sinatra.exe &
+cd ~/Desktop/FlounderMSE/kw_sims_test/TestingSims/testsims_old/SplittingSimsTest/runnewest50sims26years/split_oldersinatra2all/sim11_20
+./sinatra.exe 
+
+wait 
+
+cd ~/Desktop/FlounderMSE/kw_sims_test/TestingSims/testsims_old/SplittingSimsTest/runnewest50sims26years/split_oldersinatra2all/sim21_30
+./sinatra.exe &
+cd ~/Desktop/FlounderMSE/kw_sims_test/TestingSims/testsims_old/SplittingSimsTest/runnewest50sims26years/split_oldersinatra2all/sim31_40
+./sinatra.exe & 
+cd ~/Desktop/FlounderMSE/kw_sims_test/TestingSims/testsims_old/SplittingSimsTest/runnewest50sims26years/split_oldersinatra2all/sim41_50
+./sinatra.exe
+
